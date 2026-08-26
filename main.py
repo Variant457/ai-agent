fcf4b9ce2bf708927715e13e505987c2d465ade3
Variant457 <michael.goldsmith@outlook.com>
2026-08-26 19:29:29 -0400

    Added LLM response capability

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key is None:
+        raise RuntimeError("API Key is missing")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+            }
+        ]
+    )
+
+    if response.usage is None:
+        raise RuntimeError("API Request Failed")
+
+    print(f"Prompt tokens: {response.usage.prompt_tokens}\nResponse tokens: {response.usage.completion_tokens}")
+    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
